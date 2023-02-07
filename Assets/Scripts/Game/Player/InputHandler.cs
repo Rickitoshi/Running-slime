fa0783082628c3db1879dacfe1833a711c1db733
Rickitoshi <ginko.98@mail.ru>
2023-02-08 00:53:38 +0300

    Rework input handler

--- Assets/Scripts/Game/Player/InputHandler.cs
+++ Assets/Scripts/Game/Player/InputHandler.cs
@@ -5,14 +5,14 @@ namespace Game.Player
 {
     public class InputHandler: ITickable
     {
-        private const float SWIPE_DEAD_ZONE = 0.003f;
-        private const float CHECK_TOUCH_TIME = 0.1f;
+        private const float SWIPE_DEAD_ZONE = 0.01f;
+        private const float CHECK_TOUCH_TIME = 0.06f;
     
         public bool IsLeftSwipe { get; private set; }
         public bool IsRightSwipe { get; private set; }
         public bool IsTouch { get; private set; }
 
-        private Vector3 _prevPos = Vector3.zero;
+        private Vector3 _startPos = Vector3.zero;
         private Vector2 _normalizeDelta =  Vector2.zero;
         private bool _isHold;
         private bool _isTouched;
@@ -24,18 +24,7 @@ namespace Game.Player
             if (Input.GetMouseButtonDown(0))
             {
                 _isHold = true;
-                _prevPos = Input.mousePosition;
-            }
-
-            if (Input.GetMouseButtonUp(0))
-            {
-                _holdTime = 0;
-                _isHold = false;
-                _prevPos = Vector3.zero;
-                
-                IsTouch = false;
-                _isTouched = false;
-                _isSwiped = false;
+                _startPos = Input.mousePosition;
             }
             
             if (_isHold)
@@ -44,45 +33,48 @@ namespace Game.Player
 
                 CalculateDelta();
                 
-                if (!_isSwiped && !_isTouched)
+                if (!_isSwiped)
                 {
-                    if (_holdTime <= CHECK_TOUCH_TIME)
+                    if (Mathf.Abs(_normalizeDelta.x) > SWIPE_DEAD_ZONE)
                     {
-                        if (_normalizeDelta != Vector2.zero)
-                        {
-                            if (Mathf.Abs(_normalizeDelta.x) > SWIPE_DEAD_ZONE)
-                            {
-                                _isSwiped = true;
+                        _isSwiped = true;
 
-                                IsRightSwipe = _normalizeDelta.x > 0;
-                                IsLeftSwipe = _normalizeDelta.x < 0;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        IsTouch = true;
-                        _isTouched = true;
+                        IsRightSwipe = _normalizeDelta.x > 0;
+                        IsLeftSwipe = _normalizeDelta.x < 0;
                     }
                 }
                 else
                 {
                     IsRightSwipe = false;
                     IsLeftSwipe = false;
-                    IsTouch = false;
+                }
+            }
+
+            if (IsTouch) IsTouch = false;
+            
+            if (Input.GetMouseButtonUp(0))
+            {
+                _holdTime = 0;
+                _isHold = false;
+                _startPos = Vector3.zero;
+
+                if (_holdTime <= CHECK_TOUCH_TIME && !_isSwiped)
+                {
+                    IsTouch = true;
                 }
                 
+                _isSwiped = false;
+                IsRightSwipe = false;
+                IsLeftSwipe = false;
             }
         }
 
         private void CalculateDelta()
         {
-            Vector3 delta = Input.mousePosition - _prevPos;
+            Vector3 delta = Input.mousePosition - _startPos;
 
             _normalizeDelta.x = delta.x / Screen.width;
             _normalizeDelta.y = delta.y / Screen.height;
-
-            _prevPos = Input.mousePosition;
         }
     }
 }
